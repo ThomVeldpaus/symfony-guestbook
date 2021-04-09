007a8486a522372d6dd628a33ad7c73b536eed14
Thom Veldpaus <thom.veldpaus.werkadres@gmail.com>
2021-04-09 19:04:00 +0200

    Made  temparary nullable for admin page functionality

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -39,7 +39,7 @@ class Comment
 
     /**
      * @ORM\ManyToOne(targetEntity=Conference::class, inversedBy="comments")
-     * @ORM\JoinColumn(nullable=false)
+     * @ORM\JoinColumn(nullable=true)
      */
     private $conference;
 
