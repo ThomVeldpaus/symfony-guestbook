1c9902ced66dd8e0a4e26e89d78fa6afd055168c
Thom Veldpaus <thom.veldpaus.werkadres@gmail.com>
2021-04-09 18:43:17 +0200

    redirect to Conference CRUD Controller on /admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,20 +2,34 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Comment;
+use App\Entity\Conference;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
     /**
+     * Because the Dashboard page is still empty, it will redirect to
+     * the Conference list page
+     *
      * @Route("/admin", name="admin")
      */
     public function index(): Response
     {
-        return parent::index();
+        /**
+         * The $routeBuilder fetches a CrudUrlGenerator build which will be used
+         * to generate the URL to the Conference CRUD controller
+         */
+        $routeBuilder = $this->get(CrudUrlGenerator::class)->build();
+        $url = $routeBuilder->setController(ConferenceCrudController::class)->generateUrl();
+
+        // return the redirect to Conference CRUD Controller
+        return $this->redirect($url);
     }
 
     public function configureDashboard(): Dashboard
@@ -26,7 +40,8 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToRoute('Back to the website', 'fas fa-home', 'homepage');
+        yield MenuItem::linkToCrud('Conferences', 'fas fa-map-marker-alt', Conference::class);
+        yield MenuItem::linkToCrud('Comments', 'fas fa-comments', Comment::class);
     }
 }
