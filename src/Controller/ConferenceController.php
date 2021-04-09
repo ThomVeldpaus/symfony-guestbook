626f83cc1598471544520e263996b313164e8687
Thom Veldpaus <thom.veldpaus.werkadres@gmail.com>
2021-04-09 19:53:29 +0200

    render twig template on / with a list of all conferences

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -7,10 +7,12 @@
 
 namespace App\Controller;
 
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
@@ -18,28 +20,19 @@ class ConferenceController extends AbstractController
      * This will route / to the controller action below the annotation
      * The controller action responds basic html body with an image
      *
-     * If you define a GET parameter 'hello' in the URL,
-     * there will be a greeting message in the response
+     * In Response this Controller action will render a twig template
+     * which will render all conferences, because the twig variable
+     * is filled with the data from $conferenceRepository->findAll()
      *
-     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \Twig\Environment $twig
+     * @param \App\Repository\ConferenceRepository $conferenceRepository
      * @return \Symfony\Component\HttpFoundation\Response
      */
     #[Route('/', name: 'homepage')]
-    public function index(Request $request): Response
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
     {
-        $greet = '';
-        if($name = $request->query->get('hello')) {
-            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
-        }
-
-        return new Response(<<<EOF
-<html>
-    <body>
-        $greet
-        <img src="/images/under-construction.gif" />
-    </body>
-</html>
-EOF
-        );
+        return new Response($twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
     }
 }
