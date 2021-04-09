f7276709e778cd1d68d5ad0dcd3b456568624897
Thom Veldpaus <thom.veldpaus.werkadres@gmail.com>
2021-04-09 16:32:48 +0200

    Added a Request parameter for a greeting message to the controller action

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -8,6 +8,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,14 +18,24 @@ class ConferenceController extends AbstractController
      * This will route / to the controller action below the annotation
      * The controller action responds basic html body with an image
      *
+     * If you define a GET parameter 'hello' in the URL,
+     * there will be a greeting message in the response
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $greet = '';
+        if($name = $request->query->get('hello')) {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
+
         return new Response(<<<EOF
 <html>
     <body>
+        $greet
         <img src="/images/under-construction.gif" />
     </body>
 </html>
