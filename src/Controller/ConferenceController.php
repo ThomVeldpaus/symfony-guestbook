249aab7b5b68a8f0198d6a80535f627dcec3eab7
Thom Veldpaus <thom.veldpaus.werkadres@gmail.com>
2021-04-10 02:25:56 +0100

    create paginering on the conference comment pages, install additional intl package

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -7,6 +7,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -43,6 +45,40 @@ class ConferenceController extends AbstractController
              * from the conferenceResopository
              */
             'conferences' => $conferenceRepository->findAll(),
+        ]));
+    }
+
+    /**
+     * This method has a particular behaviour, we inject the Entity Conference in tbe method
+     * and Symfony is smart enough to load the corresponding Conference with the {id}
+     *
+     * In the Response the CommentRepository will find all matching comments by Conference
+     *
+     * To make pages of the comments you give the Paginator to twig
+     *
+     * @param Environment $twig
+     * @param Conference $conference
+     * @param CommentRepository $commentRepository
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    #[Route('/conference/{id}', name: 'conference')]
+    public function show(Request $request, Environment $twig, Conference $conference, CommentRepository $commentRepository): Response
+    {
+        // the offset is the Int parameter that will given in the Request, or default 0
+        $offset = max(0, $request->query->getInt('offset', 0));
+        // the paginator is the returned Object of my custom method to fetch one in CommentRepository
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
+        // Now the paginator is given trough to Twig and also is a next and previous value that is calculated
+        // from the current Requests $offset
+        return new Response($twig->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
         ]));
     }
 }
--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -3,10 +3,18 @@
 namespace App\Repository;
 
 use App\Entity\Comment;
+use App\Entity\Conference;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 /**
+ * The CommentRepository is the class where you can filter out
+ * certain comments by filter parameters, or even get all of
+ * the comments at once.
+ *
+ * It's also possible to fetch just one result, the first match
+ *
  * @method Comment|null find($id, $lockMode = null, $lockVersion = null)
  * @method Comment|null findOneBy(array $criteria, array $orderBy = null)
  * @method Comment[]    findAll()
@@ -14,10 +22,37 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class CommentRepository extends ServiceEntityRepository
 {
+    // This constant will decide the max amount of comments per page
+    public const PAGINATOR_PER_PAGE = 2;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Comment::class);
     }
+
+    /**
+     * To have pages with the comments, I give Doctrine a Paginator
+     * instead of a collection.
+     *
+     * @param Conference $conference
+     * @param int $offset
+     * @return Paginator
+     */
+    public function getCommentPaginator(Conference $conference, int $offset): Paginator
+    {
+        // This query selects all comments by parameters and filters
+        $query = $this->createQueryBuilder('c')
+        ->andWhere('c.conference = :conference')
+        ->setParameter('conference', $conference)
+        ->orderBy('c.createdAt', 'DESC')
+        ->setMaxResults(self::PAGINATOR_PER_PAGE)
+        ->setFirstResult($offset)
+        ->getQuery();
+
+        //The Paginator returns the result of the queryBuilder in a Paginated manner
+        return new Paginator($query);
+    }
+
 
     // /**
     //  * @return Comment[] Returns an array of Comment objects
