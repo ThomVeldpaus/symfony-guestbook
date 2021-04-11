a292a3ca65cc59616e709697de4aac5ff27895f4
Thom Veldpaus <thom.veldpaus.werkadres@gmail.com>
2021-04-11 09:45:12 +0200

    Generated an Admin user and ran a doctrine:migration:migrate

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -163,8 +163,6 @@ class ConferenceController extends AbstractController
             // get the Comment entity ready for db and fetch id
             $this->entityManager->persist($comment);
 
-            $this->entityManager->flush();
-
             // dispatch the CommentMessage with comment id and some context
             $this->bus->dispatch(new CommentMessage($comment->getId(), [
                 'photoFilename' => $filename,
